bdb67ec88bea4780a3399128ac0a19118abe731b
Hugo-Romm <64986946+Hugo-Romm@users.noreply.github.com>
2025-10-13 18:36:39 -0300

    Limpieza de duplicados y zips

--- consultoria-main/consultoria-main/app/propiedades/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask_login import login_required
-
-bp = Blueprint('propiedades', __name__)
-
-@bp.route('/')
-@login_required
-def index():
-    return 'Propiedades (en construcción)'
--- consultoria/consultoria2/app/vencimientos/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('vencimientos', __name__, url_prefix='/vencimientos')
-
-from . import routes
